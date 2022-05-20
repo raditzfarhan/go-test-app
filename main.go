7de0a722d23856801b86a4844f997182f30ae5dc
Farhan <raditzfarhan@gmail.com>
2022-05-20 23:48:51 +0800

    Pointer

--- main.go
+++ main.go
@@ -11,13 +11,24 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	// ask user for their name
+	var firstName string
+	var lastName string
+	var email string	
 	var userTickets int
+	
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
 
-	userName = "Tom"
-	userTickets = 2
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
 
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of ticket(s): ")
+	fmt.Scan(&userTickets)
+		
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
 }
