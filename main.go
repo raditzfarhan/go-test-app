ae9deaf2a5d992ce90402de7804e34071af6d9f0
Farhan <raditzfarhan@gmail.com>
2022-05-21 09:14:36 +0800

    Array and slices

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName string = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	var bookings = []string{}
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
@@ -14,7 +15,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string	
-	var userTickets int
+	var userTickets uint
 	
 	// ask user for their name
 	fmt.Println("Enter your first name: ")
@@ -28,7 +29,18 @@ func main() {
 
 	fmt.Println("Enter number of ticket(s): ")
 	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName + " " + lastName)
+
+	fmt.Printf("The whole slice: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Slice type: %T\n", bookings)
+	fmt.Printf("Slice length: %v\n", len(bookings))
 		
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v", bookings)
 
 }
